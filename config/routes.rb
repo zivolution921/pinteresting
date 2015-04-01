b0152180671eff0f541097062de8addda24507cf
ziv <ziv921@yahoo.com>
2015-04-01 19:03:21 -0400

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Pinteresting::Application.routes.draw do
-  root "pages#home"
-  get "about" => "pages#about" # creates about_path
+Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
