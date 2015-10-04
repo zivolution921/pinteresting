ea218d5b05a77f95a358a6e39741519fb60523e9
ziv <ziv921@yahoo.com>
2015-10-04 02:22:50 -0400

    Umplemented Devise for Users

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'        
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.5.2'
 
 
 	group  :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+#Required for Heroku
+#Note to set this to your host
+   config.action_mailer.default_url_options = { :host => 'dating4cause.herokuapp.com' }
+
 end
